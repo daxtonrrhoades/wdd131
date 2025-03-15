dd96ce4589885d4d58f557136dcd6162b2c38b30
DaxtonRRhoades <daxtonrrhoades@gmail.com>
2025-03-15 17:05:26 -0600

    edited the temple album project and made improvements

--- scripts/temples.js
+++ scripts/temples.js
@@ -11,10 +11,10 @@ lastModified.textContent = oLastModif.toLocaleString();
 
 
 // nav edits below here
-const mainnav = document.querySelector('navigation');
+const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 
 hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('show')
-    hambutton.classList.toggle('show')
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
 });
