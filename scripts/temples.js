6da239032e39d6a699b402390020546d617537c4
DaxtonRRhoades <daxtonrrhoades@gmail.com>
2025-03-15 16:44:40 -0600

    created temple album project

---
+++ scripts/temples.js
@@ -0,0 +1,20 @@
+// footer content below
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+let oLastModif = new Date(document.lastModified);
+let lastModified = document.getElementById("lastModified");
+lastModified.textContent = oLastModif.toLocaleString();
+
+
+// nav edits below here
+const mainnav = document.querySelector('navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show')
+    hambutton.classList.toggle('show')
+});
