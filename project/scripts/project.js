ed445abf8dd3c6b69811f8cfc8f8aa01a6959c5e
DaxtonRRhoades <daxtonrrhoades@gmail.com>
2025-04-10 19:56:20 -0600

    created Wicketpedia final project

---
+++ project/scripts/project.js
@@ -0,0 +1,17 @@
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+let oLastModif = new Date(document.lastModified);
+let lastModified = document.getElementById("lastModified");
+lastModified.textContent = oLastModif.toLocaleString();
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
